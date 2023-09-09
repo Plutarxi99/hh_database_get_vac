0c0f020c0cb20344b04c13911f310b9655ba1928
Egor Shievanov <shievanov@bk.ru>
2023-09-10 02:03:42 +0300

    ok

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import psycopg2
 
-from src import class_dbmanager, class_get_data
+from src import class_dbmanager, class_get_data, function, config
+
 
 if __name__ == '__main__':
-    list_company = ["Юнипро", "СберБанк", "РОСАТОМ", "Газпром нефть", "Okko", "NVIDIA", "Huawei", "Bayer", "Зарубежнефть", "Usetech"]
-    data = class_get_data.GetDataHH(list_company)
-    print(data.get_data_json())
+    list_company = ["Юнипро", "СберБанк", "РОСАТОМ", "Газпром нефть", "Азбука вкуса", "МегаФон", "Магнит", "Bayer", "Зарубежнефть", "Металлоинвест"]
+    # q = class_get_data.GetDataHH(list_company)
+    # print(q)
+    # for x in q:
+    #     print(x)
+
+    db_name = 'db_hh'
+    par = config.config()
+    function.create_database(db_name, params=par)
+    q = class_get_data.GetDataHH(list_company)
+    for x in q:
+        x
+    l = q.list_data
+    function.write_in_database(l, db_name, params=par)
